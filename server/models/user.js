2cd06dba22bb65afd70e80b82f2ee59a48426360
Abdurrahman Elkousy <abdelrahman_omar_1515@yahoo.com>
2019-02-01 18:20:18 +0200

    added authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,20 @@ UserSchema.methods.generateAuthToken = function() {
     //return a promise having 'token' as success argument
     return user.save().then( () => token)
 }
+UserSchema.statics.findByToken = function(token){
+    const User = this
+    let decoded
+    try {
+        decoded = jwt.verify(token,"abc123")
+    } catch(e) {
+        return Promise.reject()
+    }
+    return User.findOne({
+        '_id' : decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
+}
 
 const User = mongoose.model('User', UserSchema );
 
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require('lodash')
 const mongoose = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const {authenticate} = require('./middleware/authenticate')
 
 
 const app = express()
@@ -85,6 +86,11 @@ app.post('/users', (req,res) => {
         .then( () => user.generateAuthToken())
         .then( token => res.header('x-auth', token).send(user))
         .catch(e => res.status(400).send(e))
+})
+
+
+app.get('/users/me' , authenticate, (req,res) => {
+    res.send(req.user)
 })
 
 app.listen(3000, () => console.log('App started on port 3000'))
